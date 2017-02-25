a6ad3ba51cbae4c4d78de4b626b00e4434f920a2
AOA <akixe.otegi@sgsmap.com>
2017-02-25 10:26:39 +0100

    conf.js points to the toyguay.com server

--- src/conf.js
+++ src/conf.js
@@ -3,7 +3,7 @@
         .module('toyguay')
         .value('CONF', {
             "SERVER_BASE": "http://localhost:9001",
-            "API_SERVER_BASE" : "http://localhost:3000/",
-            "API_BASE" : "http://localhost:3000/api/v1/"
+            "API_SERVER_BASE" : "http://toyguay.com/",
+            "API_BASE" : "http://toyguay.com/api/v1/"
         })
 })();
