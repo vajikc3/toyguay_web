79c3333a8d3b7a8dc2141e03853faf602280eb8f
AOA <akixe.otegi@sgsmap.com>
2017-02-25 10:26:04 +0100

    Sends JWT token in every http call

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-(function() {
 ;(function() {
     angular
         .module('toyguay', [
@@ -11,10 +10,10 @@
 
     angular
         .module('toyguay')
-        .config( function ($locationProvider, $httpProvider) {
+        .config( function ($locationProvider, $httpProvider, jwtInterceptorProvider, jwtOptionsProvider) {
             $locationProvider.html5Mode(true)
 
-
+            // Configuración urlencode para enviar objetos json vía $http.post
             $httpProvider.defaults.transformRequest = function(data){
                 if (data === undefined) {
                     return data;
@@ -22,7 +21,18 @@
                 return $.param(data);
             }
 
-        })
+            // Configuración jwtInterceptorProvider -- Envia token en cada request
+            jwtOptionsProvider.config({
+                urlParam: 'token',
+                whiteListedDomains: ['localhost']
+            });
+
+            jwtInterceptorProvider.tokenGetter = function(store) {
+                console.log("jwtInterceptor");
+                return store.get('jwt');
+            }
+            $httpProvider.interceptors.push('jwtInterceptor');
+        });
 
     angular
         .module('toyguay')
