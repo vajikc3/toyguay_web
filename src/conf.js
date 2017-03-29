7590e48123e25fa56b559cc08a335331af3fd4fc
Ubuntu <ubuntu@toyguay.com>
2017-03-29 04:37:02 +0000

    Quits conf.js from git repo

--- src/conf.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    angular
-        .module('toyguay')
-        .value('CONF', {
-            "SERVER_BASE": "http://localhost:9001",
-            "API_SERVER_BASE" : "http://localhost:3000/",
-            "API_BASE" : "http://localhost:3000/api/v1/"
-        })
-})();
